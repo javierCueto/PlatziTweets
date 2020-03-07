22ab82b94b6a054c619d4fa098240a757cc49b12
Javier <javelie@outlook.com>
2020-03-07 16:58:08 -0700

    Initial Commit

---
+++ PlatziTweets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlatziTweets
+//
+//  Created by José Javier Cueto Mejía on 07/03/20.
+//  Copyright © 2020 José Javier Cueto Mejía. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
