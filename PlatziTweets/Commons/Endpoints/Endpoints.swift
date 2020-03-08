0447c9d8a2d487fd1efd2a312a36f3f31b73832c
Javier <javelie@outlook.com>
2020-03-08 12:35:25 -0700

    fix connection part 2

---
+++ PlatziTweets/Commons/Endpoints/Endpoints.swift
@@ -0,0 +1,18 @@
+//
+//  Endpoint.swift
+//  PlatziTweets
+//
+//  Created by José Javier Cueto Mejía on 08/03/20.
+//  Copyright © 2020 José Javier Cueto Mejía. All rights reserved.
+//
+
+import Foundation
+
+struct Endpoints{
+    static let domain = "https://platzi-tweets-backend.herokuapp.com/api/v1"
+    static let login = Endpoints.domain + "/auth"
+    static let register = Endpoints.domain + "/register"
+    static let getPosts = Endpoints.domain + "/posts"
+    static let post = Endpoints.domain + "/posts"
+    static let delete = Endpoints.domain + "/posts/"
+}
