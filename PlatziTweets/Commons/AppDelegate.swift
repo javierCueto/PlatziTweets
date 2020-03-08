171542aaa65f28b167f7a20aac4e0aba3bb554d6
Javier <javelie@outlook.com>
2020-03-07 18:01:59 -0700

    Login interface

---
+++ PlatziTweets/Commons/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  PlatziTweets
+//
+//  Created by José Javier Cueto Mejía on 07/03/20.
+//  Copyright © 2020 José Javier Cueto Mejía. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+   
+
+}
+
